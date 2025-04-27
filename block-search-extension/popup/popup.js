20d9b68b19b94fe9f2179e526d0015ac80e0e77d
Mr. Koder <aa6035283@gmail.com>
2025-04-27 15:07:01 +0000

    Update with manual changes

--- block-search-extension/popup/popup.js
+++ block-search-extension/popup/popup.js
@@ -2,6 +2,8 @@ let contentScriptReady = false;
 let searchBuffer = [];
 let activeTabId = null;
   
+const validUrls = ["*://*.appinventor.mit.edu/*", "*://ai2.appinventor.mit.edu/*"]
+
   // Function to send message to the content script
   function sendMessageToContentScript(message, callback) {
       if (activeTabId) {
@@ -40,13 +42,27 @@ let activeTabId = null;
       }
   });
   
+    // Function to inject the content script if it's not present
+  function injectContentScript(tabId) {
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      files: ['content_scripts/content.js']
+    }, () => {
+      if (chrome.runtime.lastError) {
+        console.error("Blockly Search: Error injecting content script.", chrome.runtime.lastError.message);
+      } else {
+        console.log("Blockly Search: Content script injected successfully.");
+      }
+    });
+  }
+
   // Function to send handshake to the content script
   function sendHandshake() {
       if (activeTabId) {
           sendMessageToContentScript({ action: 'handshake' }, (response) => {
-              if (chrome.runtime.lastError) {
-                console.error("Blockly Search: handshake message could not be sent", chrome.runtime.lastError.message);
-              }
+            if (chrome.runtime.lastError) {
+              console.error("Blockly Search: handshake message could not be sent", chrome.runtime.lastError.message);
+            }
           });
       }
   }
@@ -54,8 +70,24 @@ let activeTabId = null;
   // Get the active tab on load
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       if (tabs.length > 0) {
-          activeTabId = tabs[0].id;
+        activeTabId = tabs[0].id;
+        const tabUrl = tabs[0].url;
+
+        console.log("Blockly Search: Active tab ID:", activeTabId);
+        console.log("Blockly Search: Active tab URL:", tabUrl);
+
+        const urlIsValid = validUrls.some((validUrl) => {
+          return new RegExp(validUrl.replace(/\*/g, '.*')).test(tabUrl);
+        });
+
+        if(!urlIsValid){
+            console.error("Blockly Search: The current tab's URL is not supported by Blockly Search");
+        }else{
           sendHandshake();
+        }
+      }else{
+        console.error("Blockly Search: No active tab found");
+        
       }
   });
   
